5cc8bc630ab5d3a63e25dc14ff20fc2026d51554
AliBalash <alihhhb1999@gmail.com>
2025-08-04 15:24:21 +0330

    remove unused routes

--- routes/web.php
+++ routes/web.php
@@ -5,19 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 
 
-// use App\Imports\CarImport;
-// use Maatwebsite\Excel\Facades\Excel;
-// Route::get('/import-cars', function () {
-//     // Masir file Excel ro moshakhas mikonid
-//     $filePath = storage_path('app/private/Cars.xlsx');
-//     // Import file Excel
-//     Excel::import(new CarImport, $filePath);
-
-//     // Return success message
-//     return 'Data has been imported successfully from the given file.';
-// });
-
-
 use App\Livewire\Reservation\ReserveCarForm;
 
 Route::get('/', ReserveCarForm::class);
